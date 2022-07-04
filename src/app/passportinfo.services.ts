d0f0b25639805a75f95ca559a05b495e476efce4
Jesus Eduardo Martinez Guerra <guerra@media-interactive.de>
2022-07-03 21:45:54 -0500

    Creation of base project of passport-checker for connecting to backend project.

--- src/app/passportinfo.services.ts
+++ src/app/passportinfo.services.ts
@@ -3,14 +3,10 @@ import { Injectable, OnInit } from '@angular/core';
 import { ResponseData } from 'src/dto/responsedata';
 
 @Injectable()
-export class PassportInfoServices implements OnInit {
+export class PassportInfoServices{
   urlWS: string = 'http://localhost:3000/';
 
   constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-
-  }
 
   getPassportInfo(origin: string, destination: string) {
     return this.http.get<ResponseData>(this.urlWS+'passportinformation/'+`${origin}/${destination}`);
